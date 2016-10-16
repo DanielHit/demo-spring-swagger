922f5105df20706569a22092efc3033a1e972cda
hulei02 <hulei02@meituan.com>
2016-10-16 10:57:57 +0800

    initial project

---
+++ src/main/java/com/razorer/DemoSpringSwaggerApplication.java
@@ -0,0 +1,12 @@
+package com.razorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringSwaggerApplication.class, args);
+	}
+}
